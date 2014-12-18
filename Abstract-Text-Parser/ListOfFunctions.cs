5782230031fbc1851fb79112040f519203ad0613
Jordan Verasamy <j.verasamy@gmail.com>
2014-12-18 11:44:05 -0500

    decoupled functions from core parsing module

---
+++ Abstract-Text-Parser/ListOfFunctions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Text_Parser
+{
+    class ListOfFunctions : Dictionary<string, Func<List<string>, dynamic>>
+    {
+        public ListOfFunctions()
+        {
+        }
+
+        public void getFunctions()
+        {
+            // (listof int) => int
+            this.Add("+", (listOfArguments) =>
+            {
+                int count = 0;
+                foreach (string x in listOfArguments)
+                {
+                    count += int.Parse(x);
+                };
+                return count;
+            });
+
+            // (listof int) => int
+            this.Add("-", (listOfArguments) =>
+            {
+                int count = listOfArguments == null ? 0 : 2 * int.Parse(listOfArguments[0]);
+                foreach (string x in listOfArguments)
+                {
+                    count -= int.Parse(x);
+                };
+                return count;
+            });
+
+            // (listof int) => boolean
+            this.Add("int-equals?", (listOfArguments) =>
+            {
+                int pivot = listOfArguments == null ? 0 : int.Parse(listOfArguments[0]);
+
+                foreach (string x in listOfArguments)
+                {
+                    if (int.Parse(x) != pivot)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+    }
+}
--- Abstract-Text-Parser/Program.cs
+++ Abstract-Text-Parser/Program.cs
@@ -10,41 +10,21 @@ namespace Abstract_Text_Parser
     {
         static void Main(string[] args)
         {
-            Dictionary<string, Func<List<dynamic>, dynamic>> functions = new Dictionary<string, Func<List<dynamic>, dynamic>>();
+            ListOfFunctions functions = new ListOfFunctions();
+            functions.getFunctions();
 
-            functions.Add("+", (listOfArguments) =>
-            {
-                int count = 0;
-                foreach (string x in listOfArguments)
-                {
-                    count += int.Parse(x);
-                };
-                return count;
-            });
-
-            functions.Add("-", (listOfArguments) =>
-            {
-                int count = listOfArguments == null ? 0 : 2 * int.Parse(listOfArguments[0]);
-                foreach (string x in listOfArguments)
-                {
-                    count -= int.Parse(x);
-                };
-                return count;
-            });
-
-            Console.WriteLine(parseSingleFunction("(+ 3 6 ayy)", functions));
+            Console.WriteLine(parseSingleFunction("(int-equals? 3 3 3)", functions));
             Console.ReadLine();
-
         }
 
-        static dynamic parseSingleFunction(string input, Dictionary<string, Func<List<dynamic>, dynamic>> functions)
+        static dynamic parseSingleFunction(string input, ListOfFunctions functions)
         {
             var inputComponents = input.Substring(1, input.Length-2).Split(' ');
-
-            int numArguments = inputComponents.Length - 1;
-            List<dynamic> listOfArguments = new List<dynamic>();
-
+            
             string functionID = inputComponents[0];
+            int numArguments = inputComponents.Length - 1;
+
+            List<string> listOfArguments = new List<string>();
 
             for (int i = 1; i <= numArguments; i++)
             {
@@ -55,6 +35,7 @@ namespace Abstract_Text_Parser
             {
                 return functions[functionID](listOfArguments);
             }
+            #region Handle Errors
             catch (Exception e)
             {
                 Console.WriteLine("ERROR! You probably didn't obey the contract for one of the functions you used.\nYou fucker.");
@@ -62,6 +43,7 @@ namespace Abstract_Text_Parser
                 Console.WriteLine("\n\nStack Trace: \n" + e.StackTrace);
                 return "";
             }
+            #endregion
         }
     }
 }
